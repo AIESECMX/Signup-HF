1d8ee986ce8d3eaab265b4c48239975245645c49
esaiesec <esuarez@aiesec.org.mx>
2020-02-10 16:05:22 -0600

    Plugin para HF

---
+++ signup-hf.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: AIESEC HOST FAMILIA GLOBAL REGISTRO
+Description: Poder llevar los registros de familia gloabl que se enceuentran en la pagina web a la plataforma de podio
+Version: 1.0.0
+Author: Gerardo Godfrey
+Author URI: https://www.linkedin.com/in/godfreycastaÃeda/
+License: GPL
+TEST: NO
+*/
+
+
+?>
