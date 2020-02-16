470f963b92610195304d7611b8237d0fd30503fe
Sergio García <sergio.garcia@aiesec.net>
2020-02-16 12:57:05 -0600

    Comments for plugin

--- signup-hf.php
+++ signup-hf.php
@@ -8,34 +8,19 @@ Author URI: https://www.linkedin.com/in/godfreycasta?eda/
 License: GPL
 TEST: YES
 */
-wp_enqueue_script('jquery');
 
 defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 
-// function load_scripts($palette = "--aiesec-color") {
-//   wp_enqueue_script('signup-main',plugins_url('js/signup_main.js',__FILE__),array('jquery'));
-
-//   wp_localize_script('signup-main','wp_data',array(
-//     'allocationUrl' => plugins_url('model/ur_allocation.php',__FILE__),
-//     'palette' => $palette,
-//   ));
-
-//   wp_enqueue_script('signup-allocations',plugins_url('js/ur_allocation1.1.js',__FILE__),array('signup-main'),null);
-//   wp_enqueue_style('purecss','https://cdnjs.cloudflare.com/ajax/libs/pure/1.0.0/pure-min.css');
-//   wp_enqueue_style('singup-style',plugins_url('css/signupform.css',__FILE__));
-// }
+function load_scripts() {
+  wp_enqueue_style('purecss','https://cdnjs.cloudflare.com/ajax/libs/pure/1.0.0/pure-min.css');
+  wp_enqueue_style('singup-style',plugins_url('css/signupform.css',__FILE__));
+}
 
 function getFormHF($product = "",$configs = null) {
   //Get File
   $form = file_get_contents('form.html',TRUE);
   
-  //Do basic replacements
-  $actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-  
-  $form = str_replace("{website_url}",$actual_link,$form);
-  $form = str_replace("{website_url}",$actual_link,$form);
   $form = str_replace("{action_url}",plugins_url('controller/signUp.php',__FILE__),$form);
-  $form = str_replace("{product}",$product,$form);
   if ($configs != null) {
     $form = str_replace($configs['recaptcha_public_test'],$configs['recaptcha_public'],$form);
   }
@@ -65,21 +50,12 @@ function getFormWithErrorHF($form,$code) {
 }
 
 //General Sign-up Form
-function signup_form_hf( $atts ) {
+function signup_form_hf() {
     load_scripts();
-
-    //This part is legacy, better leave it as is
-    $a = shortcode_atts( array(
-        'program' => '',
-    ), $atts );
 
     $configs = include('config.php');
     $form = getFormHF("",$configs);
     
-    //Legacy: Better leave it "as is"
-    if(isset($_GET["thank_you"]) && $_GET["thank_you"]==="true") {
-      return $configs["thank-you-message"]; 
-    }
     if (isset($_REQUEST['error'])) {
       return getFormWithErrorHF($form,$_REQUEST['error']);    
     }
